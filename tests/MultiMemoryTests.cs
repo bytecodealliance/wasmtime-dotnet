c2387dc7bbfec75164911604447fbe4a1887c697
Peter Huene <peter@huene.dev>
2021-07-08 16:15:28 -0700

    Add `WithMultiMemory` to `Config`.
    This commit adds the `WithMultiMemory` method to `Config` to enable support for
    the multi-memory feature.
    
    Fixes #65.

---
+++ tests/MultiMemoryTests.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace Wasmtime.Tests
+{
+    public class MultiMemoryTests
+    {
+        [Fact]
+        public void ItFailsWithoutMultiMemoryEnabled()
+        {
+            using var engine = new Engine();
+            Action action = () =>
+            {
+                using var module = Module.FromText(engine, "test", @"(module (memory 0 1) (memory 0 1))");
+            };
+
+            action
+                .Should()
+                .Throw<WasmtimeException>()
+                .WithMessage("WebAssembly module 'test' is not valid: WebAssembly failed to compile*");
+        }
+
+        [Fact]
+        public void ItSucceedsWithMultiMemoryEnabled()
+        {
+            using var engine = new Engine(new Config().WithMultiMemory(true));
+            using var module = Module.FromText(engine, "test", @"(module (memory 0 1) (memory 0 1))");
+        }
+    }
+}
