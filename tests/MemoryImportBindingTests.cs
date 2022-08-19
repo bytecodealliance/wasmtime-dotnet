9dc5679b0852b6d2af710c86bf78efec8d3e4cd8
Martin Evans <martindevans@gmail.com>
2022-08-19 16:42:54 +0100

    Fixed import binding tests

--- tests/MemoryImportBindingTests.cs
+++ tests/MemoryImportBindingTests.cs
@@ -49,43 +49,43 @@ namespace Wasmtime.Tests
             var readFloat64 = instance.GetFunction(Store, "ReadFloat64");
             var readIntPtr = instance.GetFunction(Store, "ReadIntPtr");
 
-            mem.ReadString(Store, 0, 11).Should().Be("Hello World");
-            int written = mem.WriteString(Store, 0, "WebAssembly Rocks!");
-            mem.ReadString(Store, 0, written).Should().Be("WebAssembly Rocks!");
+            mem.ReadString(0, 11).Should().Be("Hello World");
+            int written = mem.WriteString(0, "WebAssembly Rocks!");
+            mem.ReadString(0, written).Should().Be("WebAssembly Rocks!");
 
-            mem.ReadByte(Store, 20).Should().Be(1);
-            mem.WriteByte(Store, 20, 11);
-            mem.ReadByte(Store, 20).Should().Be(11);
+            mem.ReadByte(20).Should().Be(1);
+            mem.WriteByte(20, 11);
+            mem.ReadByte(20).Should().Be(11);
             readByte.Invoke(Store).Should().Be(11);
 
-            mem.ReadInt16(Store, 21).Should().Be(2);
-            mem.WriteInt16(Store, 21, 12);
-            mem.ReadInt16(Store, 21).Should().Be(12);
+            mem.ReadInt16(21).Should().Be(2);
+            mem.WriteInt16(21, 12);
+            mem.ReadInt16(21).Should().Be(12);
             readInt16.Invoke(Store).Should().Be(12);
 
-            mem.ReadInt32(Store, 23).Should().Be(3);
-            mem.WriteInt32(Store, 23, 13);
-            mem.ReadInt32(Store, 23).Should().Be(13);
+            mem.ReadInt32(23).Should().Be(3);
+            mem.WriteInt32(23, 13);
+            mem.ReadInt32(23).Should().Be(13);
             readInt32.Invoke(Store).Should().Be(13);
 
-            mem.ReadInt64(Store, 27).Should().Be(4);
-            mem.WriteInt64(Store, 27, 14);
-            mem.ReadInt64(Store, 27).Should().Be(14);
+            mem.ReadInt64(27).Should().Be(4);
+            mem.WriteInt64(27, 14);
+            mem.ReadInt64(27).Should().Be(14);
             readInt64.Invoke(Store).Should().Be(14);
 
-            mem.ReadSingle(Store, 35).Should().Be(5);
-            mem.WriteSingle(Store, 35, 15);
-            mem.ReadSingle(Store, 35).Should().Be(15);
+            mem.ReadSingle(35).Should().Be(5);
+            mem.WriteSingle(35, 15);
+            mem.ReadSingle(35).Should().Be(15);
             readFloat32.Invoke(Store).Should().Be(15);
 
-            mem.ReadDouble(Store, 39).Should().Be(6);
-            mem.WriteDouble(Store, 39, 16);
-            mem.ReadDouble(Store, 39).Should().Be(16);
+            mem.ReadDouble(39).Should().Be(6);
+            mem.WriteDouble(39, 16);
+            mem.ReadDouble(39).Should().Be(16);
             readFloat64.Invoke(Store).Should().Be(16);
 
-            mem.ReadIntPtr(Store, 48).Should().Be((IntPtr)7);
-            mem.WriteIntPtr(Store, 48, (IntPtr)17);
-            mem.ReadIntPtr(Store, 48).Should().Be((IntPtr)17);
+            mem.ReadIntPtr(48).Should().Be((IntPtr)7);
+            mem.WriteIntPtr(48, (IntPtr)17);
+            mem.ReadIntPtr(48).Should().Be((IntPtr)17);
             readIntPtr.Invoke(Store).Should().Be(17);
         }
 
