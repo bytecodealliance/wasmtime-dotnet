c37cfd6e6c9609fabba0071277ee5da0170246f0
Bas Geertsema <mail@basgeertsema.net>
2025-06-30 12:55:31 +0200

    Upgrade to 34.0.1
    Breaking changes to C-api:
    
    * Layout and size of structs in extern.h has changed
    * Trap codes have been added and the enum value for OutOfFuel has changed from 11 to 12
    
    Due to externs having different sizes, we can no longer
    used the same ConcurrentDictionary for caching externals
    because the key size differs. Different dictionaries
    are now used depending on the extern type.

--- src/Store.cs
+++ src/Store.cs
@@ -339,52 +339,44 @@ namespace Wasmtime
         private object? data;
 
         private static readonly Native.Finalizer Finalizer = (p) => GCHandle.FromIntPtr(p).Free();
-
-        private readonly ConcurrentDictionary<(ExternKind kind, ulong store, nuint __private), object> _externCache = new();
+        
+        // The caches below use the external struct type as key type. These structs contain
+        // __private fields, but these are not interpreted and merely used for value comparison.
+        private readonly ConcurrentDictionary<ExternFunc, Function> _externFunctionCache = new();
+        private readonly ConcurrentDictionary<ExternMemory, Memory> _externMemoryCache = new();
+        private readonly ConcurrentDictionary<ExternGlobal, Global> _externGlobalCache = new();
 
         internal Function GetCachedExtern(ExternFunc @extern)
         {
-            // We use a `ValueTuple` as key, consisting of the extern type and the both
-            // struct fields, which works since all `Extern...` structs have the same
-            // fields.
-            // Even though the second field is named "__private", it should be Ok to
-            // access it since we won't interpret the value in any way, but just use it
-            // to compare it to other values.
-            var key = (ExternKind.Func, @extern.store, @extern.__private);
-
-            if (!_externCache.TryGetValue(key, out var func))
+            if (!_externFunctionCache.TryGetValue(@extern, out var func))
             {
                 func = new Function(this, @extern);
-                func = _externCache.GetOrAdd(key, func);
+                func = _externFunctionCache.GetOrAdd(@extern, func);
             }
 
-            return (Function)func;
+            return func;
         }
 
         internal Memory GetCachedExtern(ExternMemory @extern)
         {
-            var key = (ExternKind.Memory, @extern.store, @extern.__private);
-
-            if (!_externCache.TryGetValue(key, out var mem))
+            if (!_externMemoryCache.TryGetValue(@extern, out var mem))
             {
                 mem = new Memory(this, @extern);
-                mem = _externCache.GetOrAdd(key, mem);
+                mem = _externMemoryCache.GetOrAdd(@extern, mem);
             }
 
-            return (Memory)mem;
+            return mem;
         }
 
         internal Global GetCachedExtern(ExternGlobal @extern)
         {
-            var key = (ExternKind.Global, @extern.store, @extern.__private);
-
-            if (!_externCache.TryGetValue(key, out var global))
+            if (!_externGlobalCache.TryGetValue(@extern, out var global))
             {
                 global = new Global(this, @extern);
-                global = _externCache.GetOrAdd(key, global);
+                global = _externGlobalCache.GetOrAdd(@extern, global);
             }
 
-            return (Global)global;
+            return global;
         }
     }
 }
