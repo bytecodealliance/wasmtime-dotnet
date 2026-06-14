928ffe58fa42d959da41b8a76d7df5fe30530115
Martin Evans <martindevans@gmail.com>
2026-06-15 00:39:36 +0100

    Improved test coverage on Linker and Externs  - Added ExternTests which force the static constructors to run, invoking the assertions embedded there  - Added LinkerTests, covering lots of null argument scenarios  - Added some new null checks into Linker.cs which were missing

---
+++ tests/ExternTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+using Xunit;
+
+namespace Wasmtime.Tests;
+
+public class ExternTests
+{
+    [Fact]
+    public void ExternFunc_StaticCheck()
+    {
+        // An assertion is made in the static constructor. Doing this forces the static constructor to run
+        Assert.Equal(default(ExternFunc), default);
+    }
+
+    [Fact]
+    public void ExternTable_StaticCheck()
+    {
+        // An assertion is made in the static constructor. Doing this forces the static constructor to run
+        Assert.Equal(default(ExternTable), default);
+    }
+
+    [Fact]
+    public void ExternMemory_StaticCheck()
+    {
+        // An assertion is made in the static constructor. Doing this forces the static constructor to run
+        Assert.Equal(default(ExternMemory), default);
+    }
+
+    [Fact]
+    public void ExternInstance_StaticCheck()
+    {
+        // An assertion is made in the static constructor. Doing this forces the static constructor to run
+        Assert.Equal(default(ExternInstance), default);
+    }
+
+    [Fact]
+    public void ExternGlobal_StaticCheck()
+    {
+        // An assertion is made in the static constructor. Doing this forces the static constructor to run
+        Assert.Equal(default(ExternGlobal), default);
+    }
+
+    [Fact]
+    public void ExternUnion_StaticCheck()
+    {
+        // An assertion is made in the static constructor. Doing this forces the static constructor to run
+        Assert.Equal(default(ExternUnion), default);
+        Assert.Equal(default(Extern), default);
+    }
+
+    [Fact]
+    public void Extern_StaticCheck()
+    {
+        // An assertion is made in the static constructor. Doing this forces the static constructor to run
+        Assert.Equal(default(Extern), default);
+    }
+}
